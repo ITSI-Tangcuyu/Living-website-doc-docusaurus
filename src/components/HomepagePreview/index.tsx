d3beed2442d7d4ef909bc30b857c4d5fdcf34dc7
JinBin Li <joysmshr@126.com>
2022-10-16 21:30:19 +0800

    fix():TokenExample component & lds.mdx

--- src/components/HomepagePreview/index.tsx
+++ src/components/HomepagePreview/index.tsx
@@ -11,6 +11,7 @@ export interface Scene {
   image: string;
   id: string;
   details: string | null;
+  overline?: string;
 }
 
 const SceneList: Scene[] = [
@@ -18,37 +19,43 @@ const SceneList: Scene[] = [
     mission_name: '地震专题',
     image: '/img/earthquake.jpg',
     id: 'earthquake',
-    details: '地震专题的场景'
+    details: '地震专题的场景',
+    overline: 'JUNE 22 2020'
   },
   {
     mission_name: '消防专题',
     image: '/img/xiaofang.png',
     id: 'xiaofang',
-    details: '消防专题的场景'
+    details: '消防专题的场景',
+    overline: 'March 15 2021'
   },
   {
     mission_name: '台风专题',
     image: '/img/typhoon.jpg',
     id: 'typhoon',
-    details: '台风专题的场景'
+    details: '台风专题的场景',
+    overline: 'May 18 2021'
   },
   {
     mission_name: '城市生命线专题',
     image: '/img/city.jpg',
     id: 'city',
-    details: '城市生命线专题的场景'
+    details: '城市生命线专题的场景',
+    overline: 'SEP 20 2021'
   },
   {
     mission_name: '洪涝专题',
     image: '/img/flood.jpg',
     id: 'flood',
-    details: '洪涝专题的场景'
+    details: '洪涝专题的场景',
+    overline: 'NOV 01 2021'
   },
   {
     mission_name: '工业安全',
     image: '/img/factory.jpg',
     id: 'factory',
-    details: '工业安全题专题的场景'
+    details: '工业安全题专题的场景',
+    overline: 'JAN 20 2022'
   },
 ]
 
@@ -60,7 +67,7 @@ const Preview = ((scene, idx) => {
           idx={idx}
           title={scene.mission_name}
           image={scene.image}
-          overline="JUNE 26, 2022"
+          overline={scene.overline}
           href={`#`}
         />
       </li>

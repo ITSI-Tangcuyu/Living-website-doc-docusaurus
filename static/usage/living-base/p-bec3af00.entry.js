a581dcee1da43efe6028f073fcbb8c9f3a273950
JinBin Li <joysmshr@126.com>
2022-11-29 11:59:23 +0800

    feat():textarea component

--- static/usage/living-base/p-bec3af00.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as r,H as l}from"./p-8d227782.js";const o=class{constructor(r){s(this,r)}render(){return r(l,null,r("slot",null))}};o.style=":host{display:block}";export{o as li_table_row}

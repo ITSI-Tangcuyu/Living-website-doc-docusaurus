a15f7c5d733bd8788445db6c3b0937866370fc12
JinBin Li <joysmshr@126.com>
2022-12-11 19:42:44 +0800

    feat():add link to base.mdx

--- src/components/global/Card/index.tsx
+++ src/components/global/Card/index.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
 import wcs from '../../../../docs/base/basedata';
-const website = 'https://www.itsi.com.cn';
+import Link from '@docusaurus/Link';
+
 const Cards = () => {
   return wcs.map((item, idx) => {
     return (
-      <div key={idx} className='col col--6'>
+      <div
+        key={idx}
+        className='col col--6'>
         <div className='col-demo'>
           <div
             className='card margin--sm'
             style={{
               backgroundColor: 'white',
               boxShadow: '0 4px 8px 0 rgba(0,0,0,0.1)',
-            }}
-          >
+            }}>
             <div>
-              <div className='card__header' style={{ padding: '1rem' }}>
+              <div
+                className='card__header'
+                style={{ padding: '1rem' }}>
                 <h2>
                   {/* <a href={`${website}/docs/base/` + item.title} target='_self'> */}
-                  {item.title}
+                  <Link
+                    to={`/docs/base/` + item.title}
+                    target='_self'>
+                    {item.title}
+                  </Link>
+
                   {/* </a> */}
                 </h2>
               </div>
 
               <div
                 className='card__body'
-                style={{ padding: '1rem', color: 'gray' }}
-              >
+                style={{ padding: '1rem', color: 'gray' }}>
                 <p>{item.subtitle}</p>
               </div>
             </div>
